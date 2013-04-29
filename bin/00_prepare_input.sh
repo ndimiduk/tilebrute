67c5ea84f636e1c0b7bc2a6184b00a9b2eef2ebf
Nick Dimiduk <ndimiduk@gmail.com>
2013-04-28 22:34:15 -0700

    make prepare_input.sh more generic

--- bin/00_prepare_input.sh
+++ bin/00_prepare_input.sh
@@ -1,15 +1,24 @@
-#!/bin/sh
+#!/bin/sh -e
 #
 # Generate a CSV from an input shapefile, reprojecetd into EPSG:4326
 # (WGS84 lat,lng coordinates). Geometries are preserved as pre-pended
 # column "WKT".
 #
 
+if [[ $1 == "--help" ]] ; then
+  echo "Usage: prepare_input.sh [input [output]]" >&2
+  echo "Prepare a shapefile for use by tile-brute. Optional [input] is the path to an" >&2
+  echo "ogr data source to be processed and output is an optional output path. In the" >&2
+  echo "absence of either, stdin and stdout are used, respectively." >&2
+  exit 1
+fi
+
+INPUT=${1-'/vsistdin/'}
+OUTPUT=${2-'/vsistdout/'}
+
 ogr2ogr                `: invoke gdal tool ogr2ogr` \
-  -progress            `: print progress as we go` \
   -t_srs epsg:4326     `: reproject the data` \
   -f CSV               `: in CSV format` \
-  WA-epsg4326.csv      `: producing output file` \
-                       `: from input file` \
-  ~/Downloads/TIGER2010BLKPOPHU-1/tabblock2010_53_pophu/tabblock2010_53_pophu.shp \
+  $OUTPUT              `: producing output file` \
+  $INPUT               `: from input file` \
   -lco GEOMETRY=AS_WKT `: including geometries as WKT`
