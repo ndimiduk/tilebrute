c342afca68423614e353d8df592977fe34bbe6dc
Nick Dimiduk <ndimiduk@gmail.com>
2013-05-18 13:26:52 -0700

    include input preparation script in bin

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ config = {
     'install_requires': ['GDAL==1.9.1', 'Shapely==1.2.17', 'mapnik2==2.1.0.5', 'mrjob==0.4', 'nose'],
     'package_dir': {'': 'src/main/python'},
     'packages' : ['tilebrute'],
-    'scripts': ['bin/tilebrute', 'bin/01_sample_input.sh',
+    'scripts': ['bin/tilebrute', 'bin/00_prepare_input.sh', 'bin/01_sample_input.sh',
                 'bin/10_simulated_hadoop.sh', 'bin/11_hadoop_local.sh',
                 'bin/12_hadoop_local_outputfmt.sh'],
     'name': 'tilebrute'
